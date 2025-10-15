8b18cab14b66881fbd1de39fe6f04611291d2b23
Dinuka Withange <dinukakeshara55@gmail.com>
2025-10-15 08:16:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,29 @@
+function skillsMember()
+{
+    var skillsMember = document.getElementById("skills-member");
+    if (skillsMember.style.display === "none") {
+        skillsMember.style.display = "block";
+    } else {
+        skillsMember.style.display = "none";
+    }
+}
+
+function experienceMember()
+{
+    var experienceMember = document.getElementById("experience-member");
+    if (experienceMember.style.display === "none") {
+        experienceMember.style.display = "block";
+    } else {
+        experienceMember.style.display = "none";
+    }
+}
+
+function educationMember()
+{
+    var educationMember = document.getElementById("education-member");
+    if (educationMember.style.display === "none") {
+        educationMember.style.display = "block";
+    } else {
+        educationMember.style.display = "none";
+    }
+}
